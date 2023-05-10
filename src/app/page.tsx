951905024f3b753927c43d621446f2780260c4b4
Caio Lucas <lienscarlet@gmail.com>
2023-05-10 20:56:48 -0300

    feat: last added design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Icon from '@/components/Icon';
+
+import { Comment } from './components/Comment';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <main className="container px-24 py-18">
+      <div className="flex items-center gap-3">
+        <Icon name="chart-line-up" className="w-8 h-8 text-green-100" />
+        <h1>Início</h1>
+      </div>
+
+      <section className="mt-10">
+        <h2 className="font-normal text-sm mb-4">Atualizações mais recentes</h2>
+        <ul className="flex flex-col gap-3">
+          <li>
+            <Comment />
+          </li>
+          <li>
+            <Comment />
+          </li>
+          <li>
+            <Comment />
+          </li>
+          <li>
+            <Comment />
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 }
