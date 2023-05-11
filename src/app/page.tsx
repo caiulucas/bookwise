bb4b3b26524f3a9dcbd9949de2b6e4495414ac53
Caio Lucas <lienscarlet@gmail.com>
2023-05-10 23:45:06 -0300

    feat: add last read & popular books

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Icon from '@/components/Icon';
 
+import { BookCard } from './components/BookCard';
 import { Comment } from './components/Comment';
+import { LastBook } from './components/LastBook';
 
 export default function Home() {
   return (
@@ -10,23 +12,61 @@ export default function Home() {
         <h1>Início</h1>
       </div>
 
-      <section className="mt-10">
-        <h2 className="font-normal text-sm mb-4">Atualizações mais recentes</h2>
-        <ul className="flex flex-col gap-3">
-          <li>
-            <Comment />
-          </li>
-          <li>
-            <Comment />
-          </li>
-          <li>
-            <Comment />
-          </li>
-          <li>
-            <Comment />
-          </li>
-        </ul>
-      </section>
+      <div className="flex gap-16 mt-10">
+        <div className="flex flex-col gap-10">
+          <section>
+            <div className="flex justify-between">
+              <h2 className="font-normal text-sm mb-4">Sua última leitura </h2>
+              <button className="font-bold text-purple-100 flex items-center gap-2">
+                Ver todas <Icon name="caret-right" />
+              </button>
+            </div>
+            <LastBook />
+          </section>
+          <section>
+            <h2 className="font-normal text-sm mb-4">
+              Atualizações mais recentes
+            </h2>
+            <ul className="flex flex-col gap-3">
+              <li>
+                <Comment />
+              </li>
+              <li>
+                <Comment />
+              </li>
+              <li>
+                <Comment />
+              </li>
+              <li>
+                <Comment />
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <aside>
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="font-normal text-sm">Livros populares</h2>
+            <button className="font-bold text-purple-100 flex items-center gap-2">
+              Ver todos <Icon name="caret-right" />
+            </button>
+          </div>
+          <ul className="flex flex-col gap-3">
+            <li>
+              <BookCard />
+            </li>
+            <li>
+              <BookCard />
+            </li>
+            <li>
+              <BookCard />
+            </li>
+            <li>
+              <BookCard />
+            </li>
+          </ul>
+        </aside>
+      </div>
     </main>
   );
 }
