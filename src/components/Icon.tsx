ea49383c3ea5b54344dc6bfdac7c838f81ee8f9c
Caio Lucas <lienscarlet@gmail.com>
2023-05-16 16:46:58 -0300

    feat: add new icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,21 +2,27 @@
 
 import {
   Binoculars,
+  BookOpen,
+  BookmarkSimple,
   CaretRight,
   ChartLineUp,
   MagnifyingGlass,
   IconProps as PhosphorIconProps,
   Star,
-  User
+  User,
+  X
 } from 'phosphor-react';
 
 const icons = {
   'chart-line-up': ChartLineUp,
   'caret-right': CaretRight,
   'magnifying-glass': MagnifyingGlass,
+  'book-open': BookOpen,
+  bookmark: BookmarkSimple,
   user: User,
   binoculars: Binoculars,
-  star: Star
+  star: Star,
+  x: X
 };
 
 export type IconName = keyof typeof icons;
