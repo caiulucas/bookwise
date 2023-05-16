6917214304aa8aff866ae3c5196c37448015b586
Caio Lucas <lienscarlet@gmail.com>
2023-05-16 16:47:08 -0300

    feat: add new rating size

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,13 +1,38 @@
 import Icon from './Icon';
 
-export function Rating() {
+interface RatingProps {
+  size?: 'sm' | 'md';
+}
+
+export function Rating({ size = 'sm' }: RatingProps) {
+  const sizeStyle = {
+    sm: 'w-4 h-4',
+    md: 'w-5 h-5'
+  };
+
   return (
     <div className="flex gap-1">
-      <Icon name="star" weight="fill" className="text-purple-100 w-4 h-4" />
-      <Icon name="star" weight="fill" className="text-purple-100 w-4 h-4" />
-      <Icon name="star" weight="fill" className="text-purple-100 w-4 h-4" />
-      <Icon name="star" weight="fill" className="text-purple-100 w-4 h-4" />
-      <Icon name="star" className="text-purple-100 w-4 h-4" />
+      <Icon
+        name="star"
+        weight="fill"
+        className={`text-purple-100 ${sizeStyle[size]}`}
+      />
+      <Icon
+        name="star"
+        weight="fill"
+        className={`text-purple-100 ${sizeStyle[size]}`}
+      />{' '}
+      <Icon
+        name="star"
+        weight="fill"
+        className={`text-purple-100 ${sizeStyle[size]}`}
+      />{' '}
+      <Icon
+        name="star"
+        weight="fill"
+        className={`text-purple-100 ${sizeStyle[size]}`}
+      />{' '}
+      <Icon name="star" className={`text-purple-100 ${sizeStyle[size]}`} />
     </div>
   );
 }
