2686388367e3f1f67688c3ac6316b20bae22bd02
Caio Lucas <lienscarlet@gmail.com>
2023-06-08 17:33:07 -0300

    feat: add lg size to avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,17 +3,23 @@ import Image, { StaticImageData } from 'next/image';
 interface AvatarProps {
   src: string | StaticImageData;
   alt?: string;
+  size?: 'md' | 'lg';
 }
 
-export function Avatar({ src, alt = 'User avatar' }: AvatarProps) {
+export function Avatar({ src, alt = 'User avatar', size = 'md' }: AvatarProps) {
+  const imgSize = size === 'md' ? 40 : 72;
+  const imgStyleSize = size === 'md' ? 'w-10 h-10' : 'w-18 h-18';
+
   return (
-    <div className="rounded-full w-10 h-10 p-px bg-gradient-to-b from-green-50 to-purple-50">
+    <div
+      className={`rounded-full ${imgStyleSize} p-px bg-gradient-to-b from-green-50 to-purple-50`}
+    >
       <Image
         className="rounded-full"
         src={src}
         alt={alt}
-        width={40}
-        height={40}
+        width={imgSize}
+        height={imgSize}
       />
     </div>
   );
